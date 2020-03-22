703b728025646e06fc8c641827dc3d5698a7fffc
ChristineSz <57232881+ChristineSz@users.noreply.github.com>
2020-03-22 22:10:19 +0200

    My OCA Labs

---
+++ src/com/oca/src/com/oca/domain/Product.java
@@ -0,0 +1,13 @@
+package com.oca.domain;
+
+public interface Product {
+	
+	 public abstract String getName(); 
+	 
+	 public abstract void setName(String n);
+	 
+	 public abstract String toString(); 
+	 
+	} 
+
+
