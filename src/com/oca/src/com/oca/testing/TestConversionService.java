65deeff3e09fd9f6c567e7311a7573575a18d245
ChristineSz <57232881+ChristineSz@users.noreply.github.com>
2020-03-22 21:07:00 +0200

    My Labs for OCA

---
+++ src/com/oca/src/com/oca/testing/TestConversionService.java
@@ -0,0 +1,24 @@
+package com.oca.testing;
+
+import com.oca.utils.ConversionService;
+
+public class TestConversionService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(ConversionService.fluidOunces(1.1f));
+		System.out.println(ConversionService.gallons(2.2f));
+		System.out.println(ConversionService.grams(30));
+		System.out.println(ConversionService.milliliters(40));
+		System.out.println(ConversionService.ounces(50));
+		System.out.println(ConversionService.pints(6.6f));
+		System.out.println(ConversionService.pounds(7.7));
+
+	
+		
+		
+	}
+
+}
